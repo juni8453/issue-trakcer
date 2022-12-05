c8dcbbb8ccde996af4a6e734ca0a608aee343c58
ByeoungJoon Jeon <juni8453@naver.com>
2022-12-05 16:28:41 +0900

    프로젝트 초기화

---
+++ src/test/java/com/team20/issuetracker/IssuetrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team20.issuetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssuetrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
